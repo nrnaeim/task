55af270fd1fa6673180d73635183f1e2a02324d0
Niemur Rahman <nrnaeim@gmail.com>
2025-12-03 01:04:19 +0600

    api base url changed

--- client/src/apis/apis.js
+++ client/src/apis/apis.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const employeeApis = axios.create({
-  baseURL: "https://interviewtask-server.onrender.com/api/v1/employees",
+  baseURL: "https://taskserver-cpst.onrender.com/api/v1/employees",
   headers: {
     Accept: "application/json",
   },
