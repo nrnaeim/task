c7f6b6172b3216299aa4bf7b27b197b750915528
Niemur Rahman <nrnaeim@gmail.com>
2025-12-03 22:21:40 +0600

    localhost is white listed to cors

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json({}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://taskclient-z1y8.onrender.com"],
+    origin: ["https://taskclient-z1y8.onrender.com", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
