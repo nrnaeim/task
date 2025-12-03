a77d44f3d039041d9c6e071d69e45962011754c4
Niemur Rahman <nrnaeim@gmail.com>
2025-12-03 19:04:42 +0600

    apis.js file renamed to employee.api.js

--- client/src/apis/apis.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const employeeApis = axios.create({
-  baseURL: "https://taskserver-cpst.onrender.com/api/v1/employees",
-  headers: {
-    Accept: "application/json",
-  },
-});
-
-export default employeeApis;
