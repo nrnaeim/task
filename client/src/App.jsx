040b8f417a57180eb19e2e6c9d67963906202994
Niemur Rahman <nrnaeim@gmail.com>
2025-12-05 06:56:29 +0600

    Context provider is removed from app. All the state is managed by now zustand and useState.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,19 @@
 import { Routes, Route, BrowserRouter } from "react-router";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import AddEmployeeForm from "./components/AddEmployeeForm/AddEmployeeForm";
-import { AppContextProvider } from "./Contexts/Contexts";
 import Home from "./pages/Home/Home";
 import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <AppContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/addemployee" element={<AddEmployeeForm />} />
-        </Routes>
-        <ToastContainer />
-      </BrowserRouter>
-    </AppContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addemployee" element={<AddEmployeeForm />} />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
   );
 }
 
