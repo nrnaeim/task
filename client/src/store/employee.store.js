ec75785be6e8cd8320121cb061ce165da88b9576
Niemur Rahman <nrnaeim@gmail.com>
2025-12-06 00:17:53 +0600

    set loading is removed cause loading state is now manged from app store dirctly

--- client/src/store/employee.store.js
+++ client/src/store/employee.store.js
@@ -9,21 +9,17 @@ const employeeStore = (set) => ({
   //Fetch all employee state
   fetchEmployees: async () => {
     try {
-      set({ loading: true });
       const response = await employeeApis.get("/");
       if (response.data?.success) {
         set({ employees: response.data?.data });
       }
     } catch (error) {
       toast.error(error.response?.data?.message || "Failed to fetch employees");
-    } finally {
-      set({ loading: false });
     }
   },
   //Add  employee state
   addEmployee: async (employee) => {
     try {
-      set({ loading: true });
       const response = await employeeApis.post("/add", employee);
       if (response.data?.success) {
         const newEmployee = response.data.data;
@@ -31,16 +27,12 @@ const employeeStore = (set) => ({
         toast.success(response.data.message || "Added successfully");
       }
     } catch (error) {
-      set({ fetchError: true });
       toast.error(error.response.data?.message || "Failed to add employee");
-    } finally {
-      set({ loading: false });
     }
   },
   //Update  employee state
   updateEmployee: async (id, employee) => {
     try {
-      set({ loading: true });
       const response = await employeeApis.put(`/update/${id}`, employee);
       if (response.data?.success) {
         const updatedEmployee = response.data.data;
@@ -52,16 +44,12 @@ const employeeStore = (set) => ({
         toast.success(response.data?.message || "Updated successfully");
       }
     } catch (error) {
-      set({ fetchError: true });
       toast.error(error.response?.data?.data?.message || "Failed to update");
-    } finally {
-      set({ loading: false });
     }
   },
   //Delete  employee state
   deleteEmployee: async ({ _id }) => {
     try {
-      set({ loading: true });
       const response = await employeeApis.delete(`/delete/${_id}`);
       if (response.data?.success) {
         set((store) => ({
@@ -71,8 +59,6 @@ const employeeStore = (set) => ({
       }
     } catch (error) {
       toast.error(error.response.data.message || "Failed to delete");
-    } finally {
-      set({ loading: false });
     }
   },
 });
